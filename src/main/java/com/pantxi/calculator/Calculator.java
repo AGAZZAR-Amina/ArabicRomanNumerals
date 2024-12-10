ff71ca0d5422484e2ceb76699a89d573fcb9c2f1
AGAZZAR-Amina <aminaagazzar7@gmail.com>
2024-12-10 23:45:26 +0100

    Ajout d'un test paramétré pour la méthode add

--- src/main/java/com/pantxi/calculator/Calculator.java
+++ src/main/java/com/pantxi/calculator/Calculator.java
@@ -3,12 +3,12 @@ package com.pantxi.calculator;
 public class Calculator {
 
     // Méthode pour additionner deux nombres
-    public  static int add(int opG, int opD) {
+    public   int add(int opG, int opD) {
         return opG + opD;
     }
 
     // Méthode pour diviser deux nombres
-    public  static int divide(int opG, int opD) {
+    public   int divide(int opG, int opD) {
         return opG / opD;
     }
 
--- src/test/java/com/pantxi/calculator/CalculatorTest.java
+++ src/test/java/com/pantxi/calculator/CalculatorTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.AfterEach;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+
 
 public class CalculatorTest {
 
@@ -28,11 +32,12 @@ public class CalculatorTest {
 
 
     @Test
-        void testAdd() {
+        void testAddClassic() {
          ops = new Calculator();
             assertEquals(5, ops.add(2, 3), "Addition de 2 et 3 devrait être 5");
             assertEquals(0, ops.add(-1, 1), "Addition de -1 et 1 devrait être 0");
         }
+
 
         @Test
         void testDivide() {
@@ -40,6 +45,20 @@ public class CalculatorTest {
             assertEquals(2, ops.divide(4, 2), "Division de 4 par 2 devrait être 2");
             assertThrows(ArithmeticException.class, () -> ops.divide(4, 0), "Division par zéro devrait lancer une exception");
         }
+
+    // Test paramétré avec différentes valeurs
+    @ParameterizedTest
+    @CsvSource({
+            "0, 1, 1",
+            "1, 2, 3",
+            "-2, 2, 0",
+            "0, 0, 0",
+            "-1, -2, -3"
+    })
+    void testAdd(int opG, int opD, int expected) {
+        assertEquals(expected,ops.add(opG, opD), "L'addition n'a pas donné le bon résultat");
     }
 
+
+}
 
